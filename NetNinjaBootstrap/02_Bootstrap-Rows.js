0bc47084737118d9bb2874f79c38be23e3c1d810
Caitlyn Tetmeyer <caitlyntetmeyer@gmail.com>
2017-06-26 18:30:46 -0400

    Added notes on Net Ninja Bootstrap videos

---
+++ NetNinjaBootstrap/02_Bootstrap-Rows.js
@@ -0,0 +1,9 @@
+/*
+
+Bootstrap Rows
+
+https://www.youtube.com/watch?v=Q6szlOTfv7M&list=PL4cUxeGkcC9g_69kOfXICzT_hZ79_td99&index=4#t=2.982007
+
+"row" is a Bootstrap class - use it whenever you want a new row.
+
+*/
